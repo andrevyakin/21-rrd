c8e3731bbbe184406e1a682f8129a687e538cea5
andrevyakin <andrevyakin@gmail.com>
2023-06-15 17:15:22 +0300

    init

--- v6/src/App.js
+++ v6/src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Navigate,
+    useParams,
+    useLocation
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => (
+    <>
+        <h1>App Layout</h1>
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<MainPage/>}/>
+                <Route path="*" element={<Navigate to="/"/>}/>
+                <Route path="/users/:userId?" element={<Users/>}/>
+                <Route path="/edit/:userId?" element={<EditUser/>}/>
+            </Routes>
+        </Router>
+    </>
+);
+
+function MainPage() {
+    let location = useLocation();
+    return (
+        <>
+            <h3>
+                <Link to="/users">Users List Page</Link>
+            </h3>
+            {location.pathname === "/" && <h1>Main Page</h1>}
+        </>
+    );
 }
 
-export default App;
+function Users() {
+    const {userId} = useParams();
+    const getUser = userId => users.find(({id}) => String(id) === userId);
+    const users = [...Array(5).keys()].map(i => ({id: i, name: `User: ${i}`}));
+    return (
+        <>
+            <h1>Users Layout</h1>
+            <h3>
+                <Link to="/">Main Page</Link>
+            </h3>
+            {userId ? (
+                <User user={getUser(userId)} userId={userId}/>
+            ) : (
+                <UserList users={users}/>
+            )}
+        </>
+    );
+}
+
+function User() {
+    const {userId} = useParams();
+    return (
+        <>
+            <h1>User Page</h1>
+            <ul>
+                <li>
+                    <h3>
+                        <Link to="/users">Users List Page</Link>
+                    </h3>
+                </li>
+                <li>
+                    <h3>
+                        <Link to={`/edit/${userId}`}>Edit this user</Link>
+                    </h3>
+                </li>
+            </ul>
+            <p> {`userId: ${userId}`} </p>
+        </>
+    );
+}
+
+function UserList({users}) {
+    return (
+        <>
+            <h1>Users List Page</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <p>
+                            <Link to={`/users/${user.id}`}>{user.name}</Link>
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+function EditUser() {
+    const {userId} = useParams();
+    return (
+        <>
+            <h1>Edit User Page</h1>
+            <ul>
+                <li>
+                    <h3>
+                        <Link to={`/users/${userId}`}>Users Profile Page</Link>
+                    </h3>
+                </li>
+                <li>
+                    <h3>
+                        <Link to={`/users/${Number(userId) + 1}`}>Another User</Link>
+                    </h3>
+                </li>
+                <li>
+                    <h3>
+                        <Link to="/users">User List page</Link>
+                    </h3>
+                </li>
+            </ul>
+            <p> {`userId: ${userId}`} </p>
+        </>
+    );
+}
